084e4fd0e671552dc511f154faea11340f605fb3
Jason <jason@Zipcoders-MacBook-Pro.local>
2022-04-06 23:13:19 -0400

    update

--- answers/exercise3.sql
+++ answers/exercise3.sql
@@ -1,2 +1,5 @@
 Write a Query that finds the correct value for the missing timezones based on shared city, state or city, state, and county.
 
+SELECT city, state, county, timezone,
+FROM zipcodes
+WHERE timezone <> 'False' or not NULL;
